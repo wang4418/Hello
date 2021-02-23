7ef8c3fce45ceb2988f358a25786fc6ae6721301
JIE <jieandroid@163.com>
2021-02-23 21:35:23 +0800

    Initial commit

---
+++ test1/src/com/jie/Hello.java
@@ -0,0 +1,15 @@
+package com.jie;
+
+/**
+ * ClassName:Hello
+ * Package:com.jie
+ * Description:每一个牛B的人物都有一段苦B的岁月，只要像傻B一样坚持，终将会牛B！
+ *
+ * @date:2021/2/23 21:31
+ * @author:VX438364246
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
